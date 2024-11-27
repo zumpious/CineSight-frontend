ffa7794260c553c78e6dce0a659a7208944a0f46
Jonas Wagner <jonas.zumpe@googlemail.com>
2024-11-27 18:17:19 +0100

    Make base api url an env variable

--- services/movie_api.ts
+++ services/movie_api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import { MovieDetail } from '@/types/movie';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+if (!API_BASE_URL) {
+  throw new Error('NEXT_PUBLIC_API_BASE_URL is not defined');
+}
 
 export const fetchMovieById = async (id: string): Promise<MovieDetail> => {
   const response = await axios.get(`${API_BASE_URL}/movies/${id}`);
